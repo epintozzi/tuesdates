3cde060614a790cd6a66c2f1acfdcad643f6a912
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-01-08 15:43:18 -0700

    Pass restaurant info through to create event form Update create event feature test to be more accurate Add sad-path feature test for event creation Add event show view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,20 +4,29 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
-    @event = Event.new()
+    @event = Event.new
+    @restaurant = Restaurant.first_or_create(name: params[:restaurant_name], yelp_id: params[:yelp_id])
+    @event.restaurant = @restaurant
+    @address = params[:address]
   end
 
   def create
     @event = Event.create(event_params)
+    @event.restaurant = Restaurant.first_or_create(restaurant_params)
     @event.user = current_user
+
+    response = Yelp.client.business(@event.restaurant.yelp_id)
+    @address = response.business.location.display_address
+
     if @event.save
       flash[:success] = "Your invitation has been sent!"
       redirect_to event_path(@event)
     else
-      flash[:failure] = "Your iniviation was not sent"
+      flash[:failure] = "Your invitation was not sent"
       render :new
     end
   end
@@ -32,6 +41,10 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:id, :group_id, :user_id, :restaurant_id, :event_start)
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :yelp_id)
   end
 
 end
--- spec/features/events/user_can_create_an_event_spec.rb
+++ spec/features/events/user_can_create_an_event_spec.rb
@@ -5,7 +5,7 @@ describe "event creation" do
     user = create(:user)
     group = create(:group)
     group.users = [user]
-    restaurant = create(:restaurant)
+    restaurant = create(:restaurant, yelp_id: "la-potranca-taqueria-denver")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit search_path
@@ -13,13 +13,16 @@ describe "event creation" do
     fill_in :location, with: "80216"
     fill_in :term, with: "tacos"
     click_on "Search"
-    click_on "La Potranca Taqueria"
+    click_on('+', match: :first)
 
     expect(current_path).to eq(new_event_path)
 
-    # fill_in "event[date]", with: "01/07/2016"
-    # fill_in "event[time]", with: "1:00"
-    # click_on "PM"
+    select "2017", from:"event[event_start(1i)]"
+    select "January", from:"event[event_start(2i)]"
+    select "12", from:"event[event_start(3i)]"
+    select "6 PM", from:"event[event_start(4i)]"
+    select "30", from:"event[event_start(5i)]"
+
     select group.name, from:"event[group_id]"
 
     click_on "Send Invitation"
@@ -28,5 +31,32 @@ describe "event creation" do
 
     expect(page).to have_content "Your invitation has been sent!"
     expect(current_path).to eq(event_path(event))
+  end
+  scenario "user sees error message if an event is not created", :vcr do
+    user = create(:user)
+    group = create(:group)
+    group.users = [user]
+    restaurant = create(:restaurant, yelp_id: "la-potranca-taqueria-denver")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit search_path
+
+    fill_in :location, with: "80216"
+    fill_in :term, with: "tacos"
+    click_on "Search"
+    click_on('+', match: :first)
+
+    expect(current_path).to eq(new_event_path)
+
+    select "2017", from:"event[event_start(1i)]"
+    select "January", from:"event[event_start(2i)]"
+    select "12", from:"event[event_start(3i)]"
+    select "6 PM", from:"event[event_start(4i)]"
+    select "30", from:"event[event_start(5i)]"
+
+    click_on "Send Invitation"
+
+    expect(page).to have_content "Your invitation was not sent"
+    expect(current_path).to eq(new_event_path)
   end
 end

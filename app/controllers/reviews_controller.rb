8fc2abcb30d4fef8cc912779c4100b5423ee66a2
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-01-08 23:02:03 -0700

    Create reviews controllers Create reviews index and new views Write feature test that a user can leave a review on a restaurant after an event has past.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,35 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @review = Review.new()
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.user = current_user
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.user = current_user
+    @review.restaurant = @restaurant
+    if @review.save
+      flash[:success] = "Thank you for submitting your review"
+      redirect_to restaurant_reviews_path(@restaurant)
+    else
+      flash[:failure] = "Your review did not save"
+      render :new
+    end
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:id, :user_id, :restaurant_id, :review_content, :stars)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   resources :groups do
     resources :group_members, only: [:new, :create]
   end
+
   resources :events, except: [:delete]
+
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
 
   get '/dashboard', to: 'dashboard#index'
 

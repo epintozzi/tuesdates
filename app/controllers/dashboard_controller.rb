347eb53791fc3eef160ea4a1354a9cefdedd850d
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-01-11 12:06:25 -0700

    Refactor search and dashboard views Add search helper methods for finding restaurant reviews Add search helper methods for finding number of tuesdaters who have visited a restaurant Style events section on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,8 @@ class DashboardController < ApplicationController
   authorize_resource class: false
 
   def index
-    @future_events = current_user.invited_events.where("event_start >= ?", Time.now)
-    @past_events = current_user.invited_events.where("event_start < ?", Time.now)
+    @future_events = current_user.invited_events.where("event_start >= ?", Time.now).limit(3).order(event_start: :asc)
+    @past_events = current_user.invited_events.where("event_start < ?", Time.now).limit(3).order(event_start: :desc)
   end
 
 end

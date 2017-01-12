c33b4ff66b8d8a6458c01ba1b6a4e773ae7c39de
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-01-11 21:58:28 -0700

    refactor group create method

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -6,14 +6,12 @@ class GroupMembersController < ApplicationController
     @group_member.user = User.find_or_create_by(user_params)
     if GroupMember.find_by(user_id: @group_member.user.id, group_id: params[:group_id])
       flash[:warning] = "#{@group_member.user.email} already belongs to #{@group_member.group.name}"
-      redirect_to group_path(@group_member.group)
     elsif @group_member.save
       flash[:success] = "#{@group_member.user.email} has been invited to join #{@group_member.group.name}"
-      redirect_to group_path(@group_member.group)
     else
       flash[:failure] = "Invitation failed. Please try again."
-      redirect_to group_path(@group_member.group)
     end
+    redirect_to group_path(@group_member.group)
   end
 
   def destroy
